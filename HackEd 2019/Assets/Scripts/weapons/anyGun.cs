be077a9f49bc2e5d236bbb9b378f9c7fe2a22069
Curtis Dale <itscurtisdale@gmail.om>
2019-01-19 14:14:00 -0700

    Gun and Shoting
    Player has a gun and can shoot.
    Added terrain and handpainted grass texture.

--- HackEd 2019/Assets/Scripts/weapons/anyGun.cs
+++ HackEd 2019/Assets/Scripts/weapons/anyGun.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class anyGun : MonoBehaviour {
+	public Transform firePoint;
+	public GameObject bullet;
+	public float bulletSpeed;
+	public float timeBetweenBullets;
+	public float bulletDamage;
+	public float bulletPenetration;
+	bool fired;
 
 	// Use this for initialization
 	void Start () {
-		
+		firePoint = this.transform.GetChild(0).transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetButton("Fire1") && !fired)
+		{
+			StartCoroutine(FireGun());
+		}
 		
+	}
+	IEnumerator FireGun ()
+
+	{
+		fired = true;
+		Instantiate(bullet, firePoint.position, firePoint.rotation);
+		yield return new WaitForSeconds(timeBetweenBullets);
+		fired = false;
 	}
 }
--- HackEd 2019/Assets/Scripts/weapons/bullet.cs
+++ HackEd 2019/Assets/Scripts/weapons/bullet.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class bullet : MonoBehaviour {
+	public float speed = 28;
+	public float damage = 10;
+	public float health = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +14,28 @@ public class bullet : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+		if (health <= 0)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
+		health -= 1;
+		if (collision.transform.tag == "enemy")
+		{
+			if (collision.gameObject.GetComponent<enemyHealth>().health > 0)
+            {
+                collision.gameObject.GetComponent<enemyHealth>().health -= damage;
+            }
+
+			if (collision.gameObject.GetComponent<enemyHealth>().health <= 0)
+			{
+				Destroy(collision.gameObject);
+			}
+
+
+		}
 	}
 }
