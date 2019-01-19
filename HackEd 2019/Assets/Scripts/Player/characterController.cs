c6ce8a3430b32d50c38d8023493ed4edd524ff50
Curtis Dale <itscurtisdale@gmail.om>
2019-01-19 14:56:33 -0700

    Add Jump and Sprint
    Added Jump and Sprint

--- HackEd 2019/Assets/Scripts/Player/characterController.cs
+++ HackEd 2019/Assets/Scripts/Player/characterController.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class characterController : MonoBehaviour {
+	[Range(0,10)]
 	public float walkingSpeed;
 	float speed;
+    [Range(1,3)]
+	public float runMultipllier;
+	bool running;
+    [Range(1,50)]
+	public float acceleration;
+    [Range(3,100)]
+	public float jumpPower;
     
 	// Use this for initialization
 	void Start () {
@@ -14,7 +22,28 @@ public class characterController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		speed = walkingSpeed;
+		if (Input.GetButtonDown("Jump"))
+		{
+			this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * (jumpPower), ForceMode.Impulse);
+		}
+
+		if (Input.GetButton("run"))
+		{
+			running = true;
+		}
+		if (!Input.GetButton("run"))
+        {
+            running = false;
+        }
+        if (running)
+		{
+			speed = Mathf.Lerp(speed, (walkingSpeed * runMultipllier), Time.deltaTime * acceleration);
+		}
+		if (!running)
+		{
+			speed = walkingSpeed;
+		}
+       
 
 		float translation = Input.GetAxis("Vertical") * speed;
 		float straffe = Input.GetAxis("Horizontal") * speed;
--- HackEd 2019/Assets/Scripts/weapons/anyGun.cs
+++ HackEd 2019/Assets/Scripts/weapons/anyGun.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class anyGun : MonoBehaviour {
 	public Transform firePoint;
 	public GameObject bullet;
+	[Range(0, 100)]
 	public float bulletSpeed;
+	[Range(0, 1)]
 	public float timeBetweenBullets;
+	[Range(0, 100)]
 	public float bulletDamage;
+	[Range(0, 10)]
 	public float bulletPenetration;
+    [Range(0,20)]
+	public float recoilSpeed;
 	bool fired;
+	public Vector3 recoilPosition;
+	Vector3 initPos;
 
 	// Use this for initialization
 	void Start () {
 		firePoint = this.transform.GetChild(0).transform;
+		initPos = this.transform.localPosition;
+        
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		//RecoilHandler
+		transform.localPosition = Vector3.Lerp(transform.localPosition, initPos, Time.deltaTime * recoilSpeed);
+
+        //FiresTheWeapon
 		if (Input.GetButton("Fire1") && !fired)
 		{
 			StartCoroutine(FireGun());
@@ -27,8 +41,13 @@ public class anyGun : MonoBehaviour {
 	IEnumerator FireGun ()
 
 	{
+		
 		fired = true;
-		Instantiate(bullet, firePoint.position, firePoint.rotation);
+		GameObject b = Instantiate(bullet, firePoint.position, firePoint.rotation);
+		b.GetComponent<bullet>().speed = bulletSpeed;
+		b.GetComponent<bullet>().damage = bulletDamage;
+		b.GetComponent<bullet>().health = bulletPenetration;
+		this.transform.localPosition = recoilPosition;
 		yield return new WaitForSeconds(timeBetweenBullets);
 		fired = false;
 	}
