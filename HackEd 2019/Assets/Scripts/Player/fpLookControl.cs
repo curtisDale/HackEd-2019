6499e35852b65b73abc15cbe6b3c5d5507f8f84c
Curtis Dale <itscurtisdale@gmail.om>
2019-01-19 12:58:38 -0700

    Added Look Controller
    The player can now look around the scene as they move. View movement is smoothed over time so that the view alignes with the axis input more like it would in real life. This makes it so that looking around is smooth and feels good on either a keyboard or a controller.

--- HackEd 2019/Assets/Scripts/Player/fpLookControl.cs
+++ HackEd 2019/Assets/Scripts/Player/fpLookControl.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class fpLookControl : MonoBehaviour {
+	Vector2 mouseLook;
+	Vector2 smoothV;
+	public float sensitivity = 5;
+	public float smoothAmount;
+	GameObject player;
 
-	// Use this for initialization
+
 	void Start () {
-		
+		player = this.transform.parent.gameObject;
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-		
+		var md = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+		md = Vector2.Scale(md, new Vector2(sensitivity * smoothAmount, sensitivity * smoothAmount));
+		smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothAmount);
+		smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothAmount);
+		mouseLook += smoothV;
+        
+		transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+		player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+		                   
 	}
 }
