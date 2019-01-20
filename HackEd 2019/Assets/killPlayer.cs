c4c41ac4fc054ec0a5d9aab973bd878ba7457ed7
Curtis Dale <itscurtisdale@gmail.om>
2019-01-20 08:07:37 -0700

    Spce Jump commits havent been working
    test, hopefully this works

---
+++ HackEd 2019/Assets/killPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killPlayer : MonoBehaviour {
+	public GameObject player;
+	Vector3 initPlayerPos;
+	public GameObject startingText;
+	public GameObject canvas;
+	public GameObject camera;
+	public Transform ship;
+
+	// Use this for initialization
+	void Start () {
+		initPlayerPos = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.transform.tag == "player")
+		{
+			player.transform.position = initPlayerPos;
+            player.GetComponent<characterController>().enabled = false;
+            player.GetComponent<Rigidbody>().useGravity = false;
+            startingText.GetComponent<startGame>().beganOnce = false;
+            canvas.SetActive(true);
+			camera.GetComponent<fpLookControl>().enabled = false;
+			player.GetComponent<characterController>().boostAmount = 1;
+			//player.GetComponent<characterController>().storedPoints += player.GetComponent<characterController>().points;
+			camera.transform.LookAt(ship);
+		}
+
+	}
+}
---
+++ HackEd 2019/Assets/storeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class storeController : MonoBehaviour {
+	public GameObject store;
+	public bool storeOpen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.U))
+        {
+			
+			storeOpen = !storeOpen;
+			store.GetComponent<Canvas>().enabled = !store.GetComponent<Canvas>().enabled;
+        }
+		if (!storeOpen)
+		{
+			Cursor.lockState = CursorLockMode.None;
+		}
+		if (storeOpen)
+        {
+			Cursor.lockState = CursorLockMode.Locked;
+        }
+
+	}
+}
